fceebc80377fd1003848a3f7d60bcf4a3ff621b7
Morgan Brethome <morgan.brethome@epitech.eu>
2026-08-13 15:17:23 +0200

    fix possible security issue

--- src/shells/Bash.cpp
+++ src/shells/Bash.cpp
@@ -4,11 +4,24 @@ void Shell::Bash::addWidget(std::shared_ptr<IWidget> widget) {
 	list.push_back(widget);
 }
 
+static std::string escape_prompt(const std::string& s) {
+	std::string out;
+	out.reserve(s.size());
+	for (char c : s) {
+		switch (c) {
+		case '\\': out += "\\\\"; break;
+		case '\'': out += "'\\''"; break;
+		default:   out += c;
+		}
+	}
+	return out;
+}
+
 std::string Shell::Bash::exportPrompt() {
-	std::string base = "export PS1=\"";
+	std::string base = "shopt -u promptvars; export PS1='";
 	for (std::shared_ptr<IWidget> w : list) {
-		base += w->render();
+		base += escape_prompt(w->render());
 	}
 
-	return base + "\"";
+	return base + "'";
 }
